03292876fd8b76cc7da2b6a3493d33cdf8378f72
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2018-01-27 00:49:17 -0800

    Updated build system.

---
+++ operations.h
@@ -0,0 +1,8 @@
+// Implement in terms of:
+//      http://www.boost.org/doc/libs/1_66_0/libs/utility/operators.htm
+
+// Think of a better name
+//  Fundamentals?
+//  Concepts?
+//  Bases?
+//  Curiously-recurring bases?
